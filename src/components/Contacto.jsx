0b5caa09edf320b58f71217c1fdaf32349a46eb5
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2023-12-05 22:11:13 -0500

    Add contact send

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -43,10 +43,10 @@ const Contacto = () => {
             
             <section className='flex w-full md:w-1/2'>
                 <div className="flex flex-row flex-wrap gap-4 mx-auto my-6">
-                    <form name="contact" method="POST" data-netlify="true">
-                        <input type="text" placeholder='Nombre'  className='w-1/3 h-14 px-2 py-4 rounded-3xl bg-[#3D3838] text-white font-inter '/>
-                        <input type="text" placeholder='Correo'  className='w-1/2 h-14 px-2 mx-4 py-4 rounded-3xl bg-[#3D3838] text-white font-inter '/>
-                        <textarea placeholder='Mensaje' className='w-[90%] px-2 py-2 bg-[#3D3838] text-white font-inter  my-2 rounded-2xl h-1/2 resize-none' />
+                    <form name="contact" action='https://submit-form.com/fc6lQAd79' method="POST" data-netlify="true">
+                        <input type="text" placeholder='Nombre' name='name'  className='w-1/3 h-14 px-2 py-4 rounded-3xl bg-[#3D3838] text-white font-inter '/>
+                        <input type="text" placeholder='Correo' name='email'  className='w-1/2 h-14 px-2 mx-4 py-4 rounded-3xl bg-[#3D3838] text-white font-inter '/>
+                        <textarea placeholder='Mensaje' name='msg' className='w-[90%] px-2 py-2 bg-[#3D3838] text-white font-inter  my-2 rounded-2xl h-1/2 resize-none' />
                         
                         <button type="submit" className=' px-6 py-3 bg-[#06D6A0] rounded-full font-semibold font-inter text-white hover:bg-[#41ac8f] active:scale-[.90] '>ENVIAR</button>
                     </form>

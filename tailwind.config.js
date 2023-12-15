d95fe89048b50ac293e90ec9924183fbb35e7c5f
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2023-12-15 14:24:36 -0500

    D

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,8 +14,8 @@ export default {
       paytoneOne: ['Paytone One'],
     },
     backgroundImage:{
-      'proyect1': "url('assets/palacioColesterol.png')",
-      'proyect2': "url('assets/fruitDetector.png')",
+      'proyect1': "url('./assets/palacioColesterol.png')",
+      'proyect2': "url('./assets/fruitDetector.png')",
 
     },
   },
